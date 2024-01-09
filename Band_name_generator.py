98631c7493b562208e9c3d99f349b1768bd5ec89
Oliver Uy <uyoliverjames@gmail.com>
2024-01-09 21:33:10 +1030

    added starter files

---
+++ Band_name_generator.py
@@ -0,0 +1,5 @@
+print("Hello, Welcome to the Band Name Generator!")
+City = input("What city did you grow up in?\n")
+Pet = input("What is the name of your first pet?\n")
+print("Your band could be",City,Pet)
+
